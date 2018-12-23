72c71f38d873322275d05f546dfa26517a1508f3
Pedro Rene Gonzalez <ElectroSpyder@users.noreply.github.com>
2018-12-23 01:59:02 -0300

    Creando componente reutili

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,41 +4,36 @@ import { NgModule } from '@angular/core';
 import { APP_ROUTES } from './app.routes';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
+
 //import { MaterialModule } from './1material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
+import { PagesModule } from './pages/pages.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
+    RegisterComponent
+    /*NopagefoundComponent,
     DashboardComponent,
     ProgressComponent,
     Graficas1Component,
     HeaderComponent,
     SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
-    RegisterComponent
+    BreadcrumbsComponent,*/
+    
+    
   ],
   imports: [
-
-
-
     BrowserModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
     APP_ROUTES,
+    PagesModule,
+    FormsModule
     //MaterialModule
   ],
   providers: [],
